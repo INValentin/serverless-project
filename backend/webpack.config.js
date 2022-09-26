bda5df22a984787e34fd75918239f472b2719ed6
Valentin <ishimwevalentin3@gmail.com>
2022-09-26 21:45:12 +0200

    added fork-ts-checker-webpack-plugin

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -2,24 +2,57 @@ const path = require('path');
 const slsw = require('serverless-webpack');
 // var nodeExternals = require('webpack-node-externals')
 
+//module.exports = {
+//  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+//  entry: slsw.lib.entries,
+//  // externals: [nodeExternals()],
+//  devtool: 'source-map',
+//  resolve: {
+//    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+//  },
+//  output: {
+//    libraryTarget: 'commonjs',
+//    path: path.join(__dirname, '.webpack'),
+//    filename: '[name].js',
+//  },
+//  target: 'node',
+//  module: {
+//    rules: [
+//      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+//      { test: /\.tsx?$/, use: 'ts-loader' },
+//    ],
+//  },
+//};
+
+
+
+// webpack.config.js
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 module.exports = {
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  entry: slsw.lib.entries,
-  // externals: [nodeExternals()],
+  context: __dirname, // to automatically find tsconfig.json
   devtool: 'source-map',
+  entry: slsw.lib.entries,
+  target: 'node',
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    extensions: [".ts", ".tsx", ".js"],
   },
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
   },
-  target: 'node',
   module: {
     rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, use: 'ts-loader' },
-    ],
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        // add transpileOnly option if you use ts-loader < 9.3.0
+         options: {
+           transpileOnly: true
+         }
+      }
+    ]
   },
+  plugins: [new ForkTsCheckerWebpackPlugin()]
 };
